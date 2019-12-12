8b4230916d5ce13f4c3dc94c68247c223ae308f8
David Torres Herrero <gsanox@gmail.com>
2019-12-12 22:25:52 +0100

    Nova activitat afegida

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # full-stack
 
+Activitats de javascript i php per a resoldre i aprendre
+
 ### Activitats per resoldre
 
 - Crear el index de la nostra web ✅
 - Solventar problema amb el script problema1.js
+- Afegit problema2.js, mes info dins del script
+  - Enllaçar-lo al index
---
+++ problema2.js
@@ -0,0 +1,22 @@
+// Crear funcións per a convertir una cadena de text en majúscules
+
+// Si tenim açó
+let cadena =
+  "el perro de san roque no tiene rabo porque un ladroncillo se lo ha robado";
+
+// Volem acó com a resultat
+let cadena_resultant =
+  "EL PERRO DE SAN ROQUE NO TIENE RABO PORQUE UN LADRONCILLO SE LO HA ROBADO";
+
+// Facil no?
+
+// Ara el problema real, donat que tenim una frase en minúscules
+// Podries convertir a majúscules correctament, es adir, primera paraula i
+// Després de punts?, te fique un exemple:
+let cadena_minuscules =
+  "en un lugar de la mancha. de cuyo nombre no quiero acordarme";
+
+let cadena_correcta =
+  "En un lugar de la mancha. De cuyo nombre no quiero acordarme.";
+
+// Una vegada aconseguit podries fer el mateix a php, creant el script i tal?
