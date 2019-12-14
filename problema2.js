c99ea7b7cb37c2248943b428342a8c0f12c2950d
rcarcole <rcarcole@hotmail.com>
2019-12-14 20:29:45 +0100

    resolt problema2

--- problema2.js
+++ problema2.js
@@ -2,21 +2,41 @@
 
 // Si tenim açó
 let cadena =
-  "el perro de san roque no tiene rabo porque un ladroncillo se lo ha robado";
+    "el perro de san roque no tiene rabo porque un ladroncillo se lo ha robado";
 
 // Volem acó com a resultat
 let cadena_resultant =
-  "EL PERRO DE SAN ROQUE NO TIENE RABO PORQUE UN LADRONCILLO SE LO HA ROBADO";
+    "EL PERRO DE SAN ROQUE NO TIENE RABO PORQUE UN LADRONCILLO SE LO HA ROBADO";
 
 // Facil no?
+
+//RESPOSTA
+cadena = cadena.toUpperCase();
 
 // Ara el problema real, donat que tenim una frase en minúscules
 // Podries convertir a majúscules correctament, es adir, primera paraula i
 // Després de punts?, te fique un exemple:
 let cadena_minuscules =
-  "en un lugar de la mancha. de cuyo nombre no quiero acordarme";
+    "en un lugar de la mancha. de cuyo nombre no quiero acordarme";
 
 let cadena_correcta =
-  "En un lugar de la mancha. De cuyo nombre no quiero acordarme.";
+    "En un lugar de la mancha. De cuyo nombre no quiero acordarme.";
+
+
+//RESPOSTA
+console.log('En un lugar de la mancha. De cuyo nombre no quiero acordarme.'.replace(/\b[e]/g, c => c.toUpperCase()));
+
+
+
+/*
+
+'your string'.replace(/\b\w/g, l => l.toUpperCase())
+
+La expresión regular coincide con la primera letra de cada palabra dentro de la cadena dada y transforma solo esa letra en mayúscula:
+
+\ b coincide con un límite de palabra (el comienzo o el final de la palabra);
+\ w coincide con el siguiente metacaracteres [a-zA-Z0-9].
+
+/*
 
 // Una vegada aconseguit podries fer el mateix a php, creant el script i tal?
